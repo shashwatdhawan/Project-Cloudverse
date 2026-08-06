8432833068df7e2c233c8d94c696ba9b724721e6
Shashwat dhawan <shashwatdhawan747@gmail.com>
2026-08-06 11:30:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2223,29 +2223,7 @@ async def steal(
 
 @bot.event
 async def on_message(message):
-    if message.author.bot:
-        return
-
-    should_reply = False
-
-    # Reply if the bot is mentioned
-    if bot.user in message.mentions:
-        should_reply = True
-
-    # Reply if someone replies to the bot's message
-    elif message.reference:
-        try:
-            replied = await message.channel.fetch_message(message.reference.message_id)
-            if replied.author.id == bot.user.id:
-                should_reply = True
-        except Exception:
-            pass
-
-    if should_reply:
-        await message.reply(
-            "Hello! AI is being set up. This is just a test.",
-            mention_author=False
-        )
+    print(f"MESSAGE RECEIVED: {message.author} -> {message.content}")
 
     await bot.process_commands(message)
 
